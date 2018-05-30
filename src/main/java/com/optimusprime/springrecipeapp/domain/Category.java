f6a41d5e1832883bd3efc08aa4c59db2218425c0
Menik <menikdisney@gmail.com>
2018-05-30 11:51:50 +0600

    JPA many to many relationship with yml

---
+++ src/main/java/com/optimusprime/springrecipeapp/domain/Category.java
@@ -0,0 +1,40 @@
+package com.optimusprime.springrecipeapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String desctrption;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDesctrption() {
+        return desctrption;
+    }
+
+    public void setDesctrption(String desctrption) {
+        this.desctrption = desctrption;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
