a26c5688b901007751aa35e40441f5a890081ab1
Menik <menikdisney@gmail.com>
2018-05-30 13:50:40 +0600

    Spring data initializing

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category(desctrption) VALUES ('American');
+INSERT INTO category(desctrption) VALUES ('Italian');
+INSERT INTO category(desctrption) VALUES ('Mexican');
+INSERT INTO category(desctrption) VALUES ('Fast Food');
+INSERT INTO unit_of_measure(descritpion) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure(descritpion) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure(descritpion) VALUES ('Cup');
+INSERT INTO unit_of_measure(descritpion) VALUES ('Pinch');
+INSERT INTO unit_of_measure(descritpion) VALUES ('Ounce');
