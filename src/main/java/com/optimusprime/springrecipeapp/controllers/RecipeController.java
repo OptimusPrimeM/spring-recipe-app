b0d762bafc2995126c9daaa9dfdf6042c62de433
Menik <menikdisney@gmail.com>
2018-06-28 14:24:31 +0600

    Handle Number Format Exception

--- src/main/java/com/optimusprime/springrecipeapp/controllers/RecipeController.java
+++ src/main/java/com/optimusprime/springrecipeapp/controllers/RecipeController.java
@@ -66,15 +66,15 @@ public class RecipeController {
         return modelAndView;
     }
 
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(NumberFormatException.class)
-//    public ModelAndView handleNumberFormat(Exception exception) {
-//        log.error("Handle not found exception");
-//        log.error(exception.getMessage());
-//        ModelAndView modelAndView = new ModelAndView();
-//
-//        modelAndView.setViewName("400error");
-//        modelAndView.addObject("exception", exception);
-//        return modelAndView;
-//    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception) {
+        log.error("Handle not found exception");
+        log.error(exception.getMessage());
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", exception);
+        return modelAndView;
+    }
 }
--- src/test/java/com/optimusprime/springrecipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/optimusprime/springrecipeapp/controllers/RecipeControllerTest.java
@@ -113,11 +113,11 @@ public class RecipeControllerTest {
                 .andExpect(view().name("404error"));
     }
 
-//    @Test
-//    public void testGetRecipeNumberFormat() throws Exception {
-//
-//        mockMvc.perform(get("/recipe/optimum/show"))
-//                .andExpect(status().isBadRequest())
-//                .andExpect(view().name("400error"));
-//    }
+    @Test
+    public void testGetRecipeNumberFormat() throws Exception {
+
+        mockMvc.perform(get("/recipe/optimum/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
 }
