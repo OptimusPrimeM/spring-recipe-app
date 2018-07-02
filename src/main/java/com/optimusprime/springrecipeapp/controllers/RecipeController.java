e26b0b78b7356485cd6685ae0eee436a0a61d4d7
Menik <menikdisney@gmail.com>
2018-07-02 12:52:14 +0600

    Data Validation with Spring MVC

--- src/main/java/com/optimusprime/springrecipeapp/controllers/RecipeController.java
+++ src/main/java/com/optimusprime/springrecipeapp/controllers/RecipeController.java
@@ -7,14 +7,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
 public class RecipeController {
 
     private final RecipeService recipeService;
+    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeForm";
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
@@ -30,11 +34,19 @@ public class RecipeController {
     @GetMapping("recipe/new")
     public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
-        return "recipe/recipeForm";
+        return RECIPE_RECIPEFORM_URL;
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command,
+                               BindingResult bindingResult) {
+          if (bindingResult.hasErrors()){
+              bindingResult.getAllErrors().forEach(objectError -> {
+                  log.debug(objectError.toString());
+              });
+              return RECIPE_RECIPEFORM_URL;
+          }
+
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
@@ -43,7 +55,7 @@ public class RecipeController {
     public String updateRecipe(@PathVariable String id, Model model) {
         log.debug("Update id:" + id + " .in the controller class");
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-        return "recipe/recipeForm";
+        return RECIPE_RECIPEFORM_URL;
     }
 
     @GetMapping("/recipe/{id}/delete")
